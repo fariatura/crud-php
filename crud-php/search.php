9d80ee937924e037ea5cbbcef50bfb8b69abb36f
Fouzia Faria <88418502+fariatura@users.noreply.github.com>
2022-11-19 22:13:24 +0600

    final commit

---
+++ crud-php/search.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Database Search</title>
+  <script>
+    $(document).ready(function() {
+      $('.search-box input[type="text"]').on("keyup input", function() {
+        /* Get input value on change */
+        var inputVal = $(this).val();
+        var resultDropdown = $(this).siblings(".result");
+        if (inputVal.length) {
+          $.get("backend-search.php", {
+            term: inputVal
+          }).done(function(data) {
+            // Display the returned data in browser
+            resultDropdown.html(data);
+          });
+        } else {
+          resultDropdown.empty();
+        }
+      });
+
+      // Set search input value on click of result item
+      $(document).on("click", ".result p", function() {
+        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+        $(this).parent(".result").empty();
+      });
+    });
+  </script>
+</head>
+
+<body>
+  <a href="index.php">Home</a>
+  <br /><br />
+
+  <div class="search-box">
+    <input type="text" autocomplete="off" placeholder="Search....." />
+    <a href="s.php">Search</a>
+    <div class="result"></div>
+  </div>
+</body>
+
+</html>
